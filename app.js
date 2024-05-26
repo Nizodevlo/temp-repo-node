1818c8e8f437a5b9e5dc63acb13a60371d747d8b
NizoDevlo <nizodevlo@gmail.com>
2024-05-26 19:14:46 +0100

    Added nodeman for selfless server running

--- app.js
+++ app.js
@@ -11,11 +11,4 @@
 // npm init -y (skip steps)
 
 
-
-const _ = require('lodash');
-
-const items = [1,[2,[3,[4]]]]
-console.log(items)
-
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+console.log('Hi')
